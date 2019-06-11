7b01187125a7c0064772188d20d0757de4f973af
Tom <thongnx6@gmail.com>
2019-06-11 18:47:47 +0800

    Initial project

---
+++ Assets/Scripts/ScenceLoader.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScenceLoader : MonoBehaviour
+{
+    public void LoadNextScence()
+    {
+        int currentScenceIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentScenceIndex + 1);
+    }
+
+    public void LoadStartScence()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
